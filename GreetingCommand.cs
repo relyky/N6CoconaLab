7bd54ac5d53c716730d07fde68db33dc12844a1d
Rely Ky <relyky@hotmail.com>
2023-12-29 17:37:12 +0800

    Minimal API style & Class-based style

---
+++ GreetingCommand.cs
@@ -0,0 +1,34 @@
+﻿using Cocona;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N6CoconaLab;
+
+/// <summary>
+/// 一個 Command class 建說只提供一個 commnd 指令。
+/// </summary>
+class GreetingCommand
+{
+  [Command("Greeting", Description = "打招呼。用 Class-based style 實作。")]
+  public Task Greeting(
+    [Option('n', Description = "指定名稱")] string name, 
+    [Option('h', Description = "說嘿")] bool hey)
+  {
+    Console.WriteLine($"{(hey ? "嘿" : "哈囉")} {name ?? "來賓"}。");
+    return Task.CompletedTask;
+  }
+
+  /// <summary>
+  /// 這不是 Command。要加 Ignore，不然 public method 預設都是 Command。
+  /// </summary>
+  [Ignore]
+  public void DoSomething() { }
+
+  /// <summary>
+  /// 這不是 Command。不是 public method 就不會默認為 Command。
+  /// </summary>
+  void NotCommand() { }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Cocona;
 using Cocona.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using N6CoconaLab;
 using N6CoconaLab.Services;
 using System.Xml.Linq;
 
@@ -13,18 +14,28 @@ builder.Services.AddScoped<RandomService>();
 
 var app = builder.Build();
 
+// 主要指令。※只能有一個。
 //app.AddCommand((string? name, bool hey) =>
 //  Console.WriteLine($"{(hey ? "嘿" : "哈囉")} {(name ?? "來賓")}。"));
 
+// 主要指令。※只能有一個。
 app.AddCommand((
   [Option("first", Description = "這是前名稱。")] string firstname,
   [Option("last", Description = "這是後名稱。")] string? lastname) =>
   Console.WriteLine($"哈囉 {firstname} {lastname}。"));
 
-app.AddCommand("greeting", (string? name, bool hey) =>
-    Console.WriteLine($"{(hey ? "嘿" : "哈囉")} {(name ?? "來賓")}。"));
+// Command 指令。Minimal API style。
+app.AddCommand("rand", (RandomService svc) => Console.WriteLine($"產生亂數 => {svc.GetRandomGuid()}"))
+   .WithDescription("產生亂數字串吧。");
 
-//app.AddCommand("math", (RandomService svc) =>
-//    Console.WriteLine($"產生亂數 => {svc.GetRandomGuid()}"));
+// Command 指令。Class-based style。
+app.AddCommands<GreetingCommand>();
+
+// Command 指令。Minimal API style。
+app.AddCommand("SayHi", (
+    [Option('n', Description = "指定名稱")] string ? name, 
+    [Option('h', Description = "說嘿")] bool hey, 
+    RandomService svc) => Console.WriteLine($"{(hey ? "嘿" : "哈囉")} {name ?? "來賓"}。 順便生成亂數 {svc.GetRandomGuid()}"))
+   .WithDescription("打招呼。用 Minimal API style 實作");
 
 app.Run();
